cb7a043cc663afb9468c7a6273d86c734c4f3b22
fuad1990dishub <prayoga.fuady@gmail.com>
2026-04-23 03:34:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ document.getElementById("formData").addEventListener("submit", function(e){
 
     const data = {
         nama: document.getElementById("nama").value,
-        nik: document.querySelectorAll("input")[1].value,
-        usaha: document.querySelectorAll("input")[2].value,
-        npwp: document.querySelectorAll("input")[3].value,
-        telepon: document.querySelectorAll("input")[4].value,
-        alamat: document.querySelector("textarea").value,
+        nik: document.getElementById("nik").value,
+        usaha: document.getElementById("usaha").value,
+        npwp: document.getElementById("npwp").value,
+        telepon: document.getElementById("telepon").value,
+        alamat: document.getElementById("alamat").value,
         link: document.getElementById("link").value
     };
 
     fetch("https://script.google.com/macros/s/AKfycbx4kMUlfH831nmur68338T37rNSXsWVqhTCmRSf-ZFTRiGNkH20DUAP4edWIt8qMv3M/exec", {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify(data)
     })
     .then(res => res.json())
     .then(res => {
         alert("Data berhasil disimpan ke Google Sheets");
+        document.getElementById("formData").reset();
     })
     .catch(err => {
         alert("Gagal menyimpan");
-        console.log(err);
+        console.error(err);
     });
 });
